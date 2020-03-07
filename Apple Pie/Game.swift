ebf742d6421777a74fa5d7ba7fea18ed34a986c4
Nicole Henson <nhenson@nicoles-macbook-pro.local>
2020-03-06 23:34:04 -0800

    created image view with different tree images, created 26 buttons to reference the alphabet, created game file for word guesser

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,35 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Nicole Henson on 3/6/20.
+//  Copyright © 2020 Joey Feazel. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.characters.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word.characters {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+    
+}
